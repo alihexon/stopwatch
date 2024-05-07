c1a3983b86126e11a747f6ccbb105e39b7b609c7
hexoN <aliakbaripour00@gmail.com>
2024-05-07 10:26:41 +0330

    Fixed a bug where resetTimer() wasn't deactivating the stopwatch

--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 let timer;
 let isTimerActive = false;
 
-const display = document.querySelector('.display');
-const startButton = document.querySelector('.start-btn');
-const stopButton = document.querySelector('.stop-btn');
-const resetButton = document.querySelector('.reset-btn');
+const display = document.querySelector(".display");
+const startButton = document.querySelector(".start-btn");
+const stopButton = document.querySelector(".stop-btn");
+const resetButton = document.querySelector(".reset-btn");
 
-startButton.addEventListener('click', startTimer);
-stopButton.addEventListener('click', stopTimer);
-resetButton.addEventListener('click', resetTimer);
+startButton.addEventListener("click", startTimer);
+stopButton.addEventListener("click", stopTimer);
+resetButton.addEventListener("click", resetTimer);
 
-const time = (JSON.parse(localStorage.getItem('time'))) || { hour: 0, minute: 0, second: 0 };
-display.innerHTML = `${time.hour.toString().padStart(2, '0')}:${time.minute.toString().padStart(2, '0')}:${time.second.toString().padStart(2, '0')}`;
+const time = JSON.parse(localStorage.getItem("time")) || {
+  hour: 0,
+  minute: 0,
+  second: 0,
+};
+display.innerHTML = `${time.hour.toString().padStart(2, "0")}:${time.minute
+  .toString()
+  .padStart(2, "0")}:${time.second.toString().padStart(2, "0")}`;
 
 function updateTimer() {
   time.second++;
@@ -19,27 +25,30 @@ function updateTimer() {
   if (time.second === 60) {
     time.second = 0;
     time.minute++;
-  } if (time.minute === 60) {
+  }
+  if (time.minute === 60) {
     time.minute = 0;
     time.hour++;
   }
-  display.innerHTML = `${time.hour.toString().padStart(2, '0')}:${time.minute.toString().padStart(2, '0')}:${time.second.toString().padStart(2, '0')}`;
-  localStorage.setItem('time', JSON.stringify(time));
+  display.innerHTML = `${time.hour.toString().padStart(2, "0")}:${time.minute
+    .toString()
+    .padStart(2, "0")}:${time.second.toString().padStart(2, "0")}`;
+  localStorage.setItem("time", JSON.stringify(time));
 }
 
 function startTimer() {
-  if  (isTimerActive === true) {
+  if (isTimerActive === true) {
     alert("The stopwatch is already active.");
-    return
-  };
+    return;
+  }
 
   isTimerActive = true;
-  timer = setInterval(updateTimer, 1000)
+  timer = setInterval(updateTimer, 1000);
 }
 
 function stopTimer() {
   isTimerActive = false;
-  clearInterval(timer)
+  clearInterval(timer);
 }
 
 function resetTimer() {
@@ -47,5 +56,6 @@ function resetTimer() {
   time.hour = 0;
   time.minute = 0;
   time.second = 0;
-  display.innerText = '00:00:00'
+  display.innerText = "00:00:00";
+  isTimerActive = false;
 }
